d666b516d6380e00d928f0789a15cca51cccf975
Syed Saif <93311161+Saif756@users.noreply.github.com>
2025-07-24 00:06:02 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,14 @@
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (userLoggedIn && debitCard && 2==3 ) {
+    console.log("You can buy the course");   
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
+
